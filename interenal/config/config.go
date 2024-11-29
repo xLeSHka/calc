f03dc51b5465942d9d2f3626723f18ab51eb3110
xLeSHka <148469104+xLeSHka@users.noreply.github.com>
2024-11-29 20:36:14 +0400

    new branch

---
+++ interenal/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+type Config struct {
+	Width  int
+	Height int
+}
+
+func New() *Config {
+	return &Config{Width: 100, Height: 50}
+}
---
+++ pkg/calculator/calculator_test.go
@@ -0,0 +1,78 @@
+package calculator_test
+
+import (
+	"testing"
+
+	"github.com/xLeSHka/calculator/pkg/calculator"
+)
+
+func TestCalc(t *testing.T) {
+	testCasesSuccess := []struct {
+		name           string
+		expression     string
+		expectedResult float64
+	}{
+		{
+			name:           "simple",
+			expression:     "1+1",
+			expectedResult: 2,
+		},
+		{
+			name:           "priority",
+			expression:     "(2+2)*2",
+			expectedResult: 8,
+		},
+		{
+			name:           "priority",
+			expression:     "2+2*2",
+			expectedResult: 6,
+		},
+		{
+			name:           "/",
+			expression:     "1/2",
+			expectedResult: 0.5,
+		},
+	}
+	for _, testCase := range testCasesSuccess {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := calculator.Calc(testCase.expression)
+			if err != nil {
+				t.Fatalf("successful case %s returns error", testCase.expression)
+			}
+			if val != testCase.expectedResult {
+				t.Fatalf("%f should be equal %f", val, testCase.expectedResult)
+			}
+		})
+	}
+	testCasesFail := []struct {
+		name        string
+		expression  string
+		expectedErr error
+	}{
+		{
+			name:       "simple",
+			expression: "1+1*",
+		},
+		{
+			name:       "priority",
+			expression: "2+2**2",
+		},
+		{
+			name:       "priority",
+			expression: "((2+2-*(2",
+		},
+		{
+			name:       "empty",
+			expression: "",
+		},
+	}
+
+	for _, testCase := range testCasesFail {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := calculator.Calc(testCase.expression)
+			if err == nil {
+				t.Fatalf("expression %s is invalid but result  %f was obtained", testCase.expression, val)
+			}
+		})
+	}
+}
